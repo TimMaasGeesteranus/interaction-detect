b463532aa636a72718ad36ca122cc1ea8da144f1
timmaasgeesteranus <tim__mg@hotmail.com>
2023-10-21 15:27:18 +0200

    flicker background to notify user

--- user_interface/scripts/content/eventListeners.js
+++ user_interface/scripts/content/eventListeners.js
@@ -6,8 +6,21 @@ document.addEventListener('keypress', (event) => {
 });
 
 document.addEventListener("listenerIntercepted", (event) => {
+    if (event.detail == "click") {
+        notifyUser();
+    }
     browser.runtime.sendMessage({
         type: "listenerIntercepted",
         content: event.detail
     })
 });
+
+function notifyUser() {
+    const originalBackgroundColor = document.body.style.backgroundColor;
+
+    document.body.style.backgroundColor = '#354172';
+
+    setTimeout(() => {
+        document.body.style.backgroundColor = originalBackgroundColor; // Change it back to the original color (e.g., white)
+    }, 100);
+}
